95a9138012abc24222e86e0d7e5b55b85f90c754
Hossain Alamin <hossainalamin@Hossains-MacBook-Pro.local>
2022-09-29 01:10:55 +0600

    finish adding sidebar

--- src/components/Singleexcercise/SingleExcersie.js
+++ src/components/Singleexcercise/SingleExcersie.js
@@ -8,7 +8,7 @@ const SingleExcersie = ({exercise}) => {
             <img src={img} alt="" />
             <div className="card-info">
                 <h3>Run Type: {name} </h3>
-                <p>{(description).slice(0,100)}...</p>
+                <p>{(description).slice(0,50)}...</p>
                 <h4>Time Required: <span className="time-required">{timeRequired} Minute</span></h4>
             </div>
             <button className='addToBtn'>Add To List</button>
